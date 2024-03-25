1355f142dd38c8e84536832acc53a7abae0f7c68
Lucas Maciel <lucasmacielcontato@gmail.com>
2024-03-24 22:49:25 -0300

    backend

---
+++ cb_viagens_api/users/models.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from datetime import date
+
+#class CustomUser(AbstractUser):
+#    created_at = models.DateField(default=date.today(), blank=True)
+#    trips = models.ForeignKey(Author, on_delete=models.CASCADE)
