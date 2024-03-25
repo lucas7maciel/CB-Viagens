137f38b2b96b2605f4c102af99b7a5f73fa22937
Lucas Maciel <lucasmacielcontato@gmail.com>
2024-03-25 11:25:42 -0300

    Style: Moving frontend to /app

--- cb_viagens_api/trips/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Trip
-
-class TripAdmin(admin.ModelAdmin):
-    # Sets model's fields that can be changed through admin's interface
-    list_display = ['id', 'name', 'price_confort', 'price_econ', 'city', 'duration', 'seat', 'bed']
-
-
-admin.site.register(Trip, TripAdmin)
